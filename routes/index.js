53aea72d928e248186d5c80a50b1c79f4505c3a1
michelle1996 <daniela.gonzalez1996@yahoo.com>
2017-05-23 14:48:49 -0600

    Datos Personales con Foto (Daniela)
    Creacion de Informacion Personal con Foto Predefinida

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Image Scrabble Game' });
+});
+
+/*get (por medio de URL), about es la ruta que se le escribe en la URL*/
+/*SI NO SE ENVIA UNA RESPUESTA AL CLIENTE, SE QUEDA GUINDADO*/
+router.get('/about', function(req, res, next){
+  res.render('about', {"msg":"Hola Mundo!!"});
+  /*res.json();
+  res.send();
+  res.download();*/
+});
+
+router.get('/contact', function(req, res, next){
+  res.render('contact', { "image": "images/IMG_20161227_150142.jpg"});
 });
 
 module.exports = router;
